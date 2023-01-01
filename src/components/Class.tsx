830b93e14bb43cd917754824dbb12f8392ab9d8d
Vishal Chaurasia <vishalbg.mz@gmail.com>
2023-01-01 19:12:31 +0530

    Add : Class component

---
+++ src/components/Class.tsx
@@ -0,0 +1,19 @@
+import { overlayStyles } from '@/global/styles';
+
+type Props = {
+  name: string;
+  image: string;
+  description?: string;
+};
+
+export default function Class({ name, image, description }: Props) {
+  return (
+    <div className='relative mx-5 inline-block h-[380px] w-[450px]'>
+      <div className={overlayStyles}>
+        <p className='text-2xl'>{name}</p>
+        <p className='mt-5'>{description}</p>
+      </div>
+      <img className='rounded-md' src={image} alt={`${image}`} />
+    </div>
+  );
+}
